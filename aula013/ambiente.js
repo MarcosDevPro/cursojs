5f63aa411499f82479db2e8b820b63dfbc366e8b
MarcosDevPro <130003852+MarcosDevPro@users.noreply.github.com>
2024-03-16 21:08:46 -0300

    estrutura básica de repetição em JS

---
+++ aula013/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 3)
+
+/*
+    var c = 1
+while (c <= 5) {
+    console.log(`Passo ${c}`)
+    c++
+*/
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
